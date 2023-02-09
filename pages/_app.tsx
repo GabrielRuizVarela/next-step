2a4fe81ca5969dbdba9c8186daf25ccbdf7b81bf
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2023-02-09 16:48:16 -0300

    Basic configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import localFont from '@next/font/local';
+import type { AppProps } from 'next/app';
+
+export const Oufit = localFont({
+  src: './fonts/fuentes background/outfit (1)/fonts/variable/Outfit[wght].ttf',
+});
+
+export const LogoFont = localFont({
+  src: './fonts/logo/fuentes logo/DM_Sans/DMSans-Bold.ttf',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} className={Oufit.className} />;
 }
